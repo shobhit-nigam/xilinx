d169c5fe0e405c83c37e9ec44fd5f0464d89747a
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2019-06-25 14:59:46 +0530

    Add files via upload

---
+++ day1/mars.py
@@ -0,0 +1,338 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+5 + 10
+
+
+# In[2]:
+
+
+print("hello world ")
+
+
+# In[3]:
+
+
+"hello world" * 2
+
+
+# In[4]:
+
+
+5 * 3
+
+
+# In[5]:
+
+
+print("hello world ")
+print("hi")
+
+
+# In[11]:
+
+
+varx = 32
+vary = 33
+print("hello world ")
+print("hi")
+print("hari"
+print("hari")
+
+
+# In[12]:
+
+
+varx
+
+
+# In[13]:
+
+
+varx = 33
+
+
+# In[14]:
+
+
+print(varx)
+
+
+# In[15]:
+
+
+vary = 44
+
+
+# In[16]:
+
+
+varz = "hi"
+
+
+# In[24]:
+
+
+print(varz)
+
+
+# In[18]:
+
+
+vary + varx 
+
+
+# In[19]:
+
+
+vary + varz 
+
+
+# In[20]:
+
+
+"hari" + "prasad"
+
+
+# In[21]:
+
+
+"hari" +  " " + "prasad"
+
+
+# In[22]:
+
+
+help(print)
+
+
+# In[23]:
+
+
+varz.capitalize()
+
+
+# In[25]:
+
+
+type(varz)
+
+
+# In[26]:
+
+
+vara = varb = varc = 23
+
+
+# In[27]:
+
+
+print(varb)
+
+
+# In[28]:
+
+
+vara, varb , varc = 23, 56, 78
+
+
+# In[29]:
+
+
+vara = 44.5
+
+
+# In[30]:
+
+
+type(vara)
+
+
+# In[31]:
+
+
+print = 55
+
+
+# In[32]:
+
+
+print("hello")
+
+
+# In[33]:
+
+
+type(print)
+
+
+# In[34]:
+
+
+del print
+
+
+# In[35]:
+
+
+type(print)
+
+
+# In[36]:
+
+
+del print
+
+
+# In[37]:
+
+
+vara, varb , varc = 23, 56, 78
+
+
+# In[38]:
+
+
+id(vara)
+
+
+# In[39]:
+
+
+id(varb)
+
+
+# In[40]:
+
+
+id(23)
+
+
+# In[41]:
+
+
+id(varc)
+
+
+# In[42]:
+
+
+varb = 23
+
+
+# In[43]:
+
+
+id(varb)
+
+
+# In[44]:
+
+
+#comment
+
+
+# In[45]:
+
+
+varz = 'hari2'
+
+
+# In[46]:
+
+
+type(varz)
+
+
+# In[47]:
+
+
+vara = 'k'
+
+
+# In[48]:
+
+
+type(vara)
+
+
+# In[49]:
+
+
+varz[1]
+
+
+# In[50]:
+
+
+varb = "don't eat"
+
+
+# In[51]:
+
+
+print(varb)
+
+
+# In[52]:
+
+
+varb = "\teja"
+
+
+# In[53]:
+
+
+print(varb)
+
+
+# In[54]:
+
+
+varb = "\\teja"
+
+
+# In[55]:
+
+
+print(varb)
+
+
+# In[56]:
+
+
+varb[9]
+
+
+# In[57]:
+
+
+varb = 'teja'
+
+
+# In[58]:
+
+
+varb[3]
+
+
+# In[59]:
+
+
+varb[-1]
+
+
+# In[60]:
+
+
+varb[-5]
+
+
+# In[ ]:
+
+
+
+

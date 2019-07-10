deee59890579adf52ef1846d10bfdbf688c4a4e1
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2019-07-10 14:41:20 +0530

    Update mail1.py

--- 24june19/day10/mail1.py
+++ 24june19/day10/mail1.py
@@ -1,1 +1,24 @@
+#send a simple mail with html body
+import smtplib
+import getpass
 
+snd = 'trainer.nigam@gmail.com'
+rcv = 'mr.nigam@gmail.com'
+psw = getpass.getpass()
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(snd, psw)
+
+msg = """From:Shobhit
+To:Gomes
+Content-type: text/html
+Subject:Second Demo
+Hey Gomes
+<br/><p align="center"> this is a new demo</p><hr/>"""
+
+
+server.sendmail(snd, rcv, msg)
+print('send')
+#server.quit()
+server.close()
